f888a99095b146867224d65355f29cf89c34972d
Kinga Janicka <50145113+KingaJanicka@users.noreply.github.com>
2020-07-03 17:05:53 +0100

    cont

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
-    'eslint:recommended',
+    'prettier:recommended',
   ],
   parserOptions: {
     ecmaFeatures: {
@@ -17,9 +17,18 @@ module.exports = {
   },
   plugins: [
     'react',
+    'prettier',
   ],
   rules: {
     'react/react-in-jsx-scope': 'off',
+
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'func-names': 'off',
+    'no-process-exit': 'off',
+    'object-shorthand': 'off',
+    'class-methods-use-this': 'off',
   },
   globals: {
     React: 'writable',
