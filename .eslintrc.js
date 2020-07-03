9b296c17d81d24810f13ed2ca2aa176c8e69eb01
Kinga Janicka <50145113+KingaJanicka@users.noreply.github.com>
2020-07-03 17:11:46 +0100

    cont

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,34 +4,29 @@ module.exports = {
     es2020: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'prettier:recommended',
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 11,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    'prettier',
-  ],
+  plugins: ["react", "prettier"],
   rules: {
-    'react/react-in-jsx-scope': 'off',
-
-    'prettier/prettier': 'error',
-    'no-unused-vars': 'warn',
-    'no-console': 'off',
-    'func-names': 'off',
-    'no-process-exit': 'off',
-    'object-shorthand': 'off',
-    'class-methods-use-this': 'off',
+    "react/react-in-jsx-scope": "off",
+    "no-unused-vars": "warn",
+    "no-console": "off",
+    "func-names": "off",
+    "no-process-exit": "off",
+    "object-shorthand": "off",
+    "class-methods-use-this": "off",
   },
   globals: {
-    React: 'writable',
+    React: "writable",
   },
-
+  "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
 };
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import PropTypes from "prop-types";
+import React from "react";
 import {
   Button,
   Container,
@@ -12,14 +12,14 @@ import {
   Responsive,
   Segment,
   ResponsiveContainer,
-} from 'semantic-ui-react';
+} from "semantic-ui-react";
 
 // import navbar from "../lib/navbar"
 // Heads up!
 // We using React Static to prerender our docs with server side rendering, this is a quite simple solution.
 // For more advanced usage please check Responsive docs under the "Usage" section.
 const getWidth = () => {
-  const isSSR = typeof window === 'undefined';
+  const isSSR = typeof window === "undefined";
 
   return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth;
 };
@@ -35,10 +35,10 @@ const HomepageHeading = ({ mobile }) => (
       content="Imagine-a-Company"
       inverted
       style={{
-        fontSize: mobile ? '2em' : '4em',
-        fontWeight: 'normal',
+        fontSize: mobile ? "2em" : "4em",
+        fontWeight: "normal",
         marginBottom: 0,
-        marginTop: mobile ? '1.5em' : '3em',
+        marginTop: mobile ? "1.5em" : "3em",
       }}
     />
     <Header
@@ -46,9 +46,9 @@ const HomepageHeading = ({ mobile }) => (
       content="Do whatever you want when you want to."
       inverted
       style={{
-        fontSize: mobile ? '1.5em' : '1.7em',
-        fontWeight: 'normal',
-        marginTop: mobile ? '0.5em' : '1.5em',
+        fontSize: mobile ? "1.5em" : "1.7em",
+        fontWeight: "normal",
+        marginTop: mobile ? "0.5em" : "1.5em",
       }}
     />
     <Button primary size="huge">
@@ -64,22 +64,22 @@ HomepageHeading.propTypes = {
 
 const HomepageLayout = () => (
   <ResponsiveContainer>
-    <Segment style={{ padding: '8em 0em' }} vertical>
+    <Segment style={{ padding: "8em 0em" }} vertical>
       <Grid container stackable verticalAlign="middle">
         <Grid.Row>
           <Grid.Column width={8}>
-            <Header as="h3" style={{ fontSize: '2em' }}>
+            <Header as="h3" style={{ fontSize: "2em" }}>
               We Help Companies and Companions
             </Header>
-            <p style={{ fontSize: '1.33em' }}>
+            <p style={{ fontSize: "1.33em" }}>
               We can give your company superpowers to do things that they never
               thought possible. Let us delight your customers and empower your
               needs... through pure data analytics.
             </p>
-            <Header as="h3" style={{ fontSize: '2em' }}>
+            <Header as="h3" style={{ fontSize: "2em" }}>
               We Make Bananas That Can Dance
             </Header>
-            <p style={{ fontSize: '1.33em' }}>
+            <p style={{ fontSize: "1.33em" }}>
               Yes that's right, you thought it was the stuff of dreams, but even
               bananas can be bioengineered.
             </p>
@@ -101,25 +101,24 @@ const HomepageLayout = () => (
       </Grid>
     </Segment>
 
-    <Segment style={{ padding: '0em' }} vertical>
+    <Segment style={{ padding: "0em" }} vertical>
       <Grid celled="internally" columns="equal" stackable>
         <Grid.Row textAlign="center">
-          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-            <Header as="h3" style={{ fontSize: '2em' }}>
+          <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
+            <Header as="h3" style={{ fontSize: "2em" }}>
               "What a Company"
             </Header>
-            <p style={{ fontSize: '1.33em' }}>
+            <p style={{ fontSize: "1.33em" }}>
               That is what they all say about us
             </p>
           </Grid.Column>
-          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-            <Header as="h3" style={{ fontSize: '2em' }}>
+          <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
+            <Header as="h3" style={{ fontSize: "2em" }}>
               "I shouldn't have gone with their competitor."
             </Header>
-            <p style={{ fontSize: '1.33em' }}>
+            <p style={{ fontSize: "1.33em" }}>
               <Image avatar src="/images/avatar/large/nan.jpg" />
               <b>Nan</b>
-              {' '}
               Chief Fun Officer Acme Toys
             </p>
           </Grid.Column>
@@ -127,12 +126,12 @@ const HomepageLayout = () => (
       </Grid>
     </Segment>
 
-    <Segment style={{ padding: '8em 0em' }} vertical>
+    <Segment style={{ padding: "8em 0em" }} vertical>
       <Container text>
-        <Header as="h3" style={{ fontSize: '2em' }}>
+        <Header as="h3" style={{ fontSize: "2em" }}>
           Breaking The Grid, Grabs Your Attention
         </Header>
-        <p style={{ fontSize: '1.33em' }}>
+        <p style={{ fontSize: "1.33em" }}>
           Instead of focusing on content creation and hard work, we have learned
           how to master the art of doing nothing by providing massive amounts of
           whitespace and generic content that can seem massive, monolithic and
@@ -146,15 +145,15 @@ const HomepageLayout = () => (
           as="h4"
           className="header"
           horizontal
-          style={{ margin: '3em 0em', textTransform: 'uppercase' }}
+          style={{ margin: "3em 0em", textTransform: "uppercase" }}
         >
           <a href="#">Case Studies</a>
         </Divider>
 
-        <Header as="h3" style={{ fontSize: '2em' }}>
+        <Header as="h3" style={{ fontSize: "2em" }}>
           Did We Tell You About Our Bananas?
         </Header>
-        <p style={{ fontSize: '1.33em' }}>
+        <p style={{ fontSize: "1.33em" }}>
           Yes I know you probably disregarded the earlier boasts as non-sequitur
           filler content, but it's really true. It took years of gene splicing
           and combinatory DNA research, but our bananas can really dance.
@@ -165,7 +164,7 @@ const HomepageLayout = () => (
       </Container>
     </Segment>
 
-    <Segment inverted vertical style={{ padding: '5em 0em' }}>
+    <Segment inverted vertical style={{ padding: "5em 0em" }}>
       <Container>
         <Grid divided inverted stackable>
           <Grid.Row>
