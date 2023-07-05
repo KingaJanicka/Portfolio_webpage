ef88f2edea948c7f371824bd576ee92f7c09a1af
Kinga Janicka <kingajanicka7@gmail.com>
2023-07-05 16:04:41 +0100

    added basic I3 page

--- pages/internetIndex.jsx
+++ pages/internetIndex.jsx
@@ -1,30 +1,39 @@
+import { Divider } from "semantic-ui-react";
 import { PageContainer } from "../lib/pageContainer";
 
 export default function InternetIndex() {
   return (
     <PageContainer>
-      The standard Lorem Ipsum passage, used since the 1500s "Lorem ipsum dolor
-      sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-      exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-      aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-      fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-      sunt in culpa qui officia deserunt mollit anim id est laborum." Section
-      1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-      accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-      illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-      explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-      odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-      voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-      quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-      eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-      voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
-      corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-      Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
-      nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas
-      nulla pariatur?"
-      https://impact.economist.com/projects/inclusive-internet-index/
+      <div style={{ padding: "2em" }}>
+        <Divider
+          as="h3"
+          className="header"
+          horizontal
+          style={{ margin: "3em 0em", textTransform: "uppercase" }}
+        >
+          Internet Index
+        </Divider>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: " center",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+              paddingBottom: "1em",
+            }}
+          >
+            <img src="/img/i3.png" style={{ height: "70vh" }} />
+            <img src="/img/i3.png" style={{ height: "70vh" }} />
+          </div>
+          <img src="/img/i3.png" style={{ height: "70vh", width: "auto" }} />
+        </div>
+      </div>
     </PageContainer>
   );
 }
